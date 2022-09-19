30a5ba94af5208064dc3b22e16b8fe8efed82d93
SeifEldinMohamed <seifeldinmohamed101@gmail.com>
2022-09-19 23:17:33 +0200

    Main Module Added

---
+++ app/src/main/java/com/seif/composeonboarding/di/MainModule.kt
@@ -0,0 +1,22 @@
+package com.seif.composeonboarding.di
+
+import android.content.Context
+import com.seif.composeonboarding.data.DataStoreRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MainModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStoreRepository(
+        @ApplicationContext context: Context
+    ) = DataStoreRepository(context = context)
+
+}
